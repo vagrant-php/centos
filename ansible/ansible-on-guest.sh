c492b38f65e23a4d4f77d00ec04834306f92f90a
Dominik Zogg <dominik.zogg@gmail.com>
2015-11-07 19:07:20 +0100

    simplify ansible installation

--- ansible/ansible-on-guest.sh
+++ ansible/ansible-on-guest.sh
@@ -4,16 +4,10 @@
 cp -f /vagrant/vagrant-php/ansible/resolv.conf /etc/resolv.conf
 
 # add repository with ansible
-rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm > /dev/null 2>&1
+yum install -y epel-release > /dev/null 2>&1
 
-# add key
-rpm --import https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7 > /dev/null 2>&1
-
-# Update apt information
-yum -y update > /dev/null 2>&1
-
-# Install ansible, https support for apt
-yum -y install ansible > /dev/null 2>&1
+# install ansible
+yum install -y ansible > /dev/null 2>&1
 
 export PYTHONUNBUFFERED=1
 
